4c3551b08695805ca7c9a37656d0548904dbd8d6
Eien_g <GSKryachko@gmail.com>
2017-12-07 00:33:09 +0500

    Added beautiful subparsers

--- main.py
+++ main.py
@@ -4,49 +4,36 @@ from cipherGenerator import CipherGenerator
 from controller import Controller
 
 parser = argparse.ArgumentParser()
-parser.add_argument('source', metavar='source', type=str,
-                    help="path to text file")
+subparsers = parser.add_subparsers(dest='goal')
 
-parser.add_argument('--decrypt', action='store_true',
-                    default=False,
-                    dest='decrypt',
-                    help='decode text')
+encrypt_parser = subparsers.add_parser('encrypt')
+encrypt_parser.add_argument('source')
+encrypt_parser.add_argument('destination')
 
-parser.add_argument('--analyze', action='store_true',
-                    default=False,
-                    dest='analyze',
-                    help='Try to get key and print it to console')
+decrypt_parser = subparsers.add_parser('decrypt')
+decrypt_parser.add_argument('source')
+decrypt_parser.add_argument('destination')
+decrypt_parser.add_argument('key')
 
-parser.add_argument('--encrypt', action='store_true',
-                    default=False,
-                    dest='encrypt',
-                    help='Try to get key and print it to console')
+build_parser = subparsers.add_parser('build')
+build_parser.add_argument('source')
+build_parser.add_argument('destination')
 
-parser.add_argument('--build', action='store_true',
-                    default=False,
-                    dest='build',
-                    help='Build masks and save to destination')
+get_key_parser = subparsers.add_parser('get_key')
+get_key_parser.add_argument('text')
+get_key_parser.add_argument('masks')
+get_key_parser.add_argument('key')
 
-parser.add_argument('--dest', metavar='destination', type=str,
-                    default='decrypted.txt',
-                    help="where to save decrypted file")
-
-parser.add_argument('--key', metavar='key', type=str,
-                    default=None,
-                    help='key to encryption/decryption')
-
-parser.add_argument('--masks', metavar='masks', type=str,
-                    default=None,
-                    help='key to encryption/decryption')
 args = parser.parse_args()
-alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-if args.encrypt:
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print(args.goal)
+if args.goal == 'encrypt':
     key = CipherGenerator.generate_cipher(alphabet)
-    Controller.encrypt(args.source, args.dest, key)
-if args.build:
-    Controller.build_masks(args.source, args.dest)
-if args.analyze:
-    Controller.get_key(args.source, args.masks, args.key)
-if args.decrypt:
-    Controller.decrypt(args.source, args.dest, args.key)
+    Controller.encrypt(args.source, args.destination, key)
+if args.goal == 'build':
+    Controller.build_masks(args.source, args.destination)
+if args.goal == 'get_key':
+    Controller.get_key(args.text, args.masks, args.key)
+if args.goal == 'decrypt':
+    Controller.decrypt(args.source, args.destination, args.key)
